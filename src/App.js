fa077631a7100525617026e00ee48600cb5d7929
ritesh singh <riteshsinghcseer@gmail.com>
2022-05-06 10:44:56 +0530

    first commit with wordSet error

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Board from "./components/Board";
+import { boardDefault, generateWordSet } from "./Words";
+import Keyboard from "./components/Keyboard";
+import { createContext, useEffect, useState } from "react";
 
+export const AppContext = createContext();
 function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+  const [wordSet, setWordSet] = useState(new Set());
+  const [disabledLetters, setDisabledLetters] = useState([]);
+  const correctWord = "RIGHT";
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordSet(words.wordSet);
+      console.log(wordSet);
+    });
+  }, []);
+
+  const onEnter = () => {
+    if (currAttempt.letterPos !== 5) return;
+    let currWord = "";
+    for (let i = 0; i < 5; i++) {
+      currWord += board[currAttempt.attempt][i];
+    }
+    if (wordSet.has(currWord.toLowerCase())) {
+      setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+    } else {
+      alert("Word Not Found");
+    }
+    if (currWord === correctWord) {
+      alert("You won!");
+      setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+    }
+  };
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos - 1 });
+  };
+  const onSelectLetter = (keyVal) => {
+    if (currAttempt.letterPos > 4) return;
+
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          currAttempt,
+          setCurrAttempt,
+          onDelete,
+          onSelectLetter,
+          onEnter,
+          correctWord,
+          setDisabledLetters,
+          disabledLetters,
+        }}
+      >
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
     </div>
   );
 }
