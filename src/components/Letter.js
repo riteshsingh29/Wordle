38426b3d4bc260bc934f0b8c54913b200d74e1a9
ritesh singh <riteshsinghcseer@gmail.com>
2022-05-06 13:00:34 +0530

    finally Project Completed

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -11,8 +11,9 @@ function Letter({ letterPos, attemptVal }) {
   } = useContext(AppContext);
   const letter = board[attemptVal][letterPos];
 
-  const correct = correctWord[letterPos] === letter;
-  const almost = !correct && letter !== "" && correctWord.includes(letter);
+  const correct = correctWord.toUpperCase()[letterPos] === letter;
+  const almost =
+    !correct && letter !== "" && correctWord.toUpperCase().includes(letter);
   const letterState =
     currAttempt.attempt > attemptVal &&
     (correct ? "correct" : almost ? "almost" : "error");
